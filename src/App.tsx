8cfae539644ceb7e83df7f5630ef43d3c1ca622e
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-10-23 19:47:47 +0200

    Create TodoStore with zustand

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import "./App.module.css";
 import { Header } from "./components/Header/Header";
 import { TodoCreator } from "./components/TodoCreator/TodoCreator";
 import { Wrapper } from "./components/Wrapper/Wrapper";
+import { useTodoStore } from "./store";
 function App() {
+  const todoList = useTodoStore((state) => state.todoList);
   return (
     <>
       <Wrapper>
         <Header />
         <TodoCreator />
       </Wrapper>
+      {todoList.map((todo) => (
+        <span>{todo.title}</span>
+      ))}
     </>
   );
 }
---
+++ src/store.ts
@@ -0,0 +1,20 @@
+import {create} from "zustand";
+
+
+type Todo = {
+    id: number,
+    title: string,
+    isComplete: boolean,
+}
+
+type TodoStore = {
+    todoList : Array<Todo>
+}
+
+
+export const useTodoStore = create<TodoStore>(() => ({
+    todoList: [
+        {id: 1, title: "Initial todo...", isComplete: true},
+        {id: 2, title: "Gardening", isComplete: false},
+        {id: 3, title: "Something", isComplete: false}]
+}))
