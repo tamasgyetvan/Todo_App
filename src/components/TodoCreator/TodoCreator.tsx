535403eba35e6b43ba7c8d32fd885835a2b2f258
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-10-24 15:15:28 +0200

    Rework with react-hook-form

--- src/components/TodoCreator/TodoCreator.tsx
+++ src/components/TodoCreator/TodoCreator.tsx
@@ -1,14 +1,27 @@
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import styles from "./todocreator.module.css";
 
 export function TodoCreator() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
   return (
-    <form className="todoCreator">
-      <input
-        className={styles.todoInput}
-        type="text"
-        name="todo"
-        placeholder="Create a new todo..."
-      />
-    </form>
+    <>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("todoTitle", { required: true })}
+          className={styles.todoInput}
+          type="text"
+          placeholder="Create a new todo..."
+        />
+        {errors.todoTitle && (
+          <span className={styles.errorMessage}>This is required!</span>
+        )}
+      </form>
+    </>
   );
 }
