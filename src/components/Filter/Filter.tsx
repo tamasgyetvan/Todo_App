63160f765c7045a2dd5a0b1ae62295be6232c734
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-10-31 09:25:32 +0100

    Refactor rendering logic

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,23 +1,31 @@
+import { useTodoStore } from "../../store";
 import styles from "./filter.module.css";
 type FilterProps = {
   handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 export function Filter({ handleClick }: FilterProps) {
+  const options = ["all", "active", "completed"];
+  const todoList = useTodoStore((state) => state.todoList);
+  const activeItems = todoList.filter((todo) => todo.isComplete != true);
+
   return (
     <section className={styles.filter}>
-      <p>5 items left</p>
+      <p>{activeItems.length} items left</p>
       <div>
-        <button type="button" data-filter="all" onClick={handleClick}>
-          All
-        </button>
-        <button type="button" data-filter="activeOnly" onClick={handleClick}>
-          Active
-        </button>
-        <button type="button" data-filter="completeOnly" onClick={handleClick}>
-          Completed
-        </button>
+        {options.map((option) => (
+          <button
+            type="button"
+            className={styles.filterButton}
+            data-filter={option}
+            onClick={handleClick}
+          >
+            {option}
+          </button>
+        ))}
       </div>
-      <button>Clear completed</button>
+      <button type="button" className={styles.clearButton}>
+        Clear completed
+      </button>
     </section>
   );
 }
