b3a2a20b046c03c2f39274d61a9b3859e3eb02df
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-10-31 12:10:37 +0100

    Add both dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
 import { useState } from "react";
-import "./App.module.css";
+import styles from "./App.module.css";
 import { Header } from "./components/Header/Header";
 import { TodoCreator } from "./components/TodoCreator/TodoCreator";
 import { TodoList } from "./components/TodoList/TodoList";
 import { Wrapper } from "./components/Wrapper/Wrapper";
 import { Filter } from "./components/Filter/Filter";
+import useLocalStorage from "use-local-storage";
 
 function App() {
   const [filter, setFilter] = useState<string | null>("all");
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark").matches;
 
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  function switchTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  }
   function handleFilterChange(e: React.MouseEvent<HTMLButtonElement>) {
     const target = e.target as HTMLButtonElement;
     setFilter(target.getAttribute("data-filter"));
   }
 
   return (
-    <>
+    <div className={styles.app} data-theme={theme}>
       <Wrapper>
-        <Header />
+        <Header theme={theme} switchTheme={switchTheme} />
         <TodoCreator />
       </Wrapper>
       <TodoList filter={filter} />
       <Filter handleClick={handleFilterChange} currentFilter={filter} />
-    </>
+    </div>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import styles from "./header.module.css";
 
-export function Header() {
+type HeaderProps = {
+  theme: string;
+  switchTheme: () => void;
+};
+export function Header({ theme, switchTheme }: HeaderProps) {
   return (
     <header className={styles.header}>
       <h1 className={styles.h1}>TODO</h1>
-      <button className={styles.button} type="button">
-        <img src="/icon-sun.svg" alt="Color Mode Toggle Button" />
+      <button className={styles.button} onClick={switchTheme} type="button">
+        <img
+          src={theme == "dark" ? "/icon-sun.svg" : "/icon-moon.svg"}
+          alt="Color Mode Toggle Button"
+        />
       </button>
     </header>
   );
